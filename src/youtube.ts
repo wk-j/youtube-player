ed73ec4589deb1bd195a1fad75447c191325d8eb
wk-j <somnuk.wk@gmail.com>
2018-07-12 02:55:08 +0700

    Update

--- src/youtube.ts
+++ src/youtube.ts
@@ -17,29 +17,17 @@ export class YouTube implements vscode.TextDocumentContentProvider {
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <link href="https://vjs.zencdn.net/7.0.5/video-js.css" rel="stylesheet">
-</head>
+<head> </head>
 
 <body>
-    <iframe src="http://localhost:5000/night.mp4"></iframe>
+    <iframe src="https://player.vimeo.com/video/277584430" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen
+        allowfullscreen></iframe>
 
     <iframe class="frame" width="560" height="315" src="https://www.youtube.com/embed/BHQfBbQBRWM?rel=0&amp;showinfo=0" frameborder="0"
         allow="autoplay; encrypted-media" allowfullscreen>
     </iframe>
-
-    <iframe src="https://www.youtube.com" width="1000" height="500">
-    </iframe>
-
-    <video id="my-video" class="video-js" controls preload="auto" width="640" height="264" data-setup="{}">
-        <source src="http://192.168.1.45:5000/night.mp4" type='video/mp4'>
-        <!--
-        <source src="night.mp4" type='video/mp4'>
-         <source src="file:///Users/wk/Source/VsCodeYoutubePlayer/night.mp4" type='video/mp4'>
-         -->
-    </video>
-    <script src="https://vjs.zencdn.net/7.0.5/video.js"></script>
 </body>
+
 </html>
         `
     }
